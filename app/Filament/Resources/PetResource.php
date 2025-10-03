7f1a1971aead10248ca235b92968964e2f136473
aintstiffen <decastro.250618@lipa.sti.edu.ph>
2025-10-03 15:05:05 +0800

    petresouser

--- app/Filament/Resources/PetResource.php
+++ app/Filament/Resources/PetResource.php
@@ -125,7 +125,7 @@ class PetResource extends Resource
 
                 // inside PetResource::form(...)
                 \App\Filament\Components\FileUpload::make('image')
-                    ->disk('public')                 // upload locally first to avoid S3 ACL issues
+                    ->disk('s3')                 // upload locally first to avoid S3 ACL issues
                     ->directory('pets/temp')         // temporary local directory
                     ->preserveFilenames()            // keep original filename
                     ->rules(['required', 'image', 'max:2048'])
