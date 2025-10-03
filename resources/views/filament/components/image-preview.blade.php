ca80119cc64297260a56210f957f7c127fc9f053
aintstiffen <decastro.250618@lipa.sti.edu.ph>
2025-10-04 01:14:04 +0800

    asdhahdsada

--- resources/views/filament/components/image-preview.blade.php
+++ resources/views/filament/components/image-preview.blade.php
@@ -14,7 +14,7 @@
                 <button 
                     type="button"
                     @click="showGrid = !showGrid"
-                    class="text-xs px-3 py-1.5 rounded bg-blue-500 hover:bg-blue-600 text-white transition-colors font-medium"
+                    class="text-xs px-3 py-1.5 rounded bg-blue-500 hover:bg-blue-600 text-blue transition-colors font-medium"
                 >
                     <span x-show="!showGrid">📐 Show Grid & Coordinates</span>
                     <span x-show="showGrid">✕ Hide Grid</span>
