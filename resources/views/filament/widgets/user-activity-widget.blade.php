9f946119899da4579561d5faece77113775f5672
aintstiffen <decastro.250618@lipa.sti.edu.ph>
2025-10-04 22:42:02 +0800

    error fixed

--- resources/views/filament/widgets/user-activity-widget.blade.php
+++ resources/views/filament/widgets/user-activity-widget.blade.php
@@ -13,18 +13,20 @@
     }
 @endphp
 
-@if($path)
-    <div class="space-y-2">
-        <div class="text-xs text-gray-600 dark:text-gray-400">Stored image</div>
-        <div class="rounded border border-gray-200 dark:border-gray-600 bg-white/50 p-2 inline-block max-w-full">
-            <img
-                src="{{ route('b2.image', ['path' => $path]) }}"
-                alt="Stored image"
-                class="max-h-48 rounded object-contain"
-                loading="lazy"
-            >
+<div>
+    @if($path)
+        <div class="space-y-2">
+            <div class="text-xs text-gray-600 dark:text-gray-400">Stored image</div>
+            <div class="rounded border border-gray-200 dark:border-gray-600 bg-white/50 p-2 inline-block max-w-full">
+                <img
+                    src="{{ route('b2.image', ['path' => $path]) }}"
+                    alt="Stored image"
+                    class="max-h-48 rounded object-contain"
+                    loading="lazy"
+                >
+            </div>
         </div>
-    </div>
-@else
-    <div class="text-xs text-gray-500">No stored image yet.</div>
-@endif
+    @else
+        <div class="text-xs text-gray-500">No stored image yet.</div>
+    @endif
+</div>
