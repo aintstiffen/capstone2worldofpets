7a9654b9e668475c6fd284acf747e3ac27975d23
aintstiffen <decastro.250618@lipa.sti.edu.ph>
2025-10-05 00:16:19 +0800

    change path

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -25,7 +25,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('admin')
-            ->path('admin')
+            ->path('worldofpets/admin/admin/hidden')
             ->login()
             ->authGuard('admin')
             ->brandName('World of Pets Admin')
